c75791d99df82f4db9e5a30dcb4945a624fdef38
Subrata Das <158165979+subratadasGit@users.noreply.github.com>
2024-04-14 13:11:54 +0000

    01

---
+++ 01_arrays.js
@@ -0,0 +1,36 @@
+// arrays
+
+const myarr = [0,1,2,3,4]
+myheros=["shaktiman","naagraaj"]
+
+const myarr2 = new Array(1,2,3,4)
+
+console.log(myarr[2]);
+console.log(myarr2);
+
+
+// arrays method
+
+myarr.push(6)
+console.log(myarr);
+
+myarr.pop()
+console.log(myarr);
+
+console.log(myarr.includes(2));
+
+console.log(myarr.indexOf(2));
+
+console.log(myarr);
+
+console.log("A ", myarr);
+
+const newarr = myarr.slice(1,3)
+console.log(newarr);
+console.log("A ", myarr);
+
+const newarr2 = myarr.splice(1,3)
+console.log(newarr2);
+console.log("B ", myarr);  //extract from original array and 
+
+
